1a2d40e5e7e00d207261bf843f36179229caa900
IMRANDIL <aliimranadil2@gmail.com>
2021-06-25 10:25:55 +0530

    Updates

---
+++ precache-manifest.02c26961fd04670bd736ce4c129d3c3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f09c9a8590494a991707e183df631ea",
+    "url": "/Grocery_Bud_React/index.html"
+  },
+  {
+    "revision": "8255f60f1803e77b1b36",
+    "url": "/Grocery_Bud_React/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/Grocery_Bud_React/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grocery_Bud_React/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8255f60f1803e77b1b36",
+    "url": "/Grocery_Bud_React/static/js/main.e1bf5213.chunk.js"
+  },
+  {
+    "revision": "d0d8b1f1df3ef1372e44",
+    "url": "/Grocery_Bud_React/static/js/runtime-main.02505f48.js"
+  }
+]);
